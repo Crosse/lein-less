9d1e6d9c53f4d45fd1f127f112604d4cde1ffb3c
Baris Yuksel <brsyuksel@gmail.com>
2020-10-07 15:33:45 +0300

    deleting unnecessary console definition, fixing graalvm js-java interop calls

--- src/leiningen/less/less-rhino-1.7.5.js
+++ src/leiningen/less/less-rhino-1.7.5.js
@@ -19,52 +19,6 @@ tree = less.tree = {};
 less.mode = 'rhino';
 
 (function() {
-    var java_type = ((typeof Java) != "undefined" && Java.type);
-
-    console = function() {
-        var stdout = java.lang.System.out;
-        var stderr = java.lang.System.err;
-
-        function doLog(out, type) {
-            return function() {
-                var args;
-                if (java_type) {
-                    args = new (Java.type("Object[]"))(arguments.length - 1);
-                } else {
-                    args = java.lang.reflect.Array.newInstance(java.lang.Object, arguments.length - 1);
-                }
-                var format = arguments[0];
-                var conversionIndex = 0;
-                // need to look for %d (integer) conversions because in Javascript all numbers are doubles
-                for (var i = 1; i < arguments.length; i++) {
-                    var arg = arguments[i];
-                    if (conversionIndex != -1) {
-                        conversionIndex = format.indexOf('%', conversionIndex);
-                    }
-                    if (conversionIndex >= 0 && conversionIndex < format.length) {
-                        var conversion = format.charAt(conversionIndex + 1);
-                        if (conversion === 'd' && typeof arg === 'number') {
-                            arg = new java.lang.Integer(new java.lang.Double(arg).intValue());
-                        }
-                        conversionIndex++;
-                    }
-                    args[i-1] = arg;
-                }
-                try {
-                    out.println(type + java.lang.String.format(format, args));
-                } catch(ex) {
-                    stderr.println(ex);
-                }
-            }
-        }
-        return {
-            log: doLog(stdout, ''),
-            info: doLog(stdout, 'INFO: '),
-            error: doLog(stderr, 'ERROR: '),
-            warn: doLog(stderr, 'WARN: ')
-        };
-    }();
-
     less.modules = {};
 
     less.modules.path = {
@@ -113,8 +67,10 @@ less.mode = 'rhino';
     less.modules.fs = {
         readFileSync: function(name) {
             // read a file into a byte array
-            var file = new java.io.File(name);
-            var stream = new java.io.FileInputStream(file);
+            var javaIOFile = Java.type("java.io.File");
+            var file = new javaIOFile(name);
+            var javaIOFileInputStream = Java.type("java.io.FileInputStream");
+            var stream = new javaIOFileInputStream(file);
             var buffer = [];
             var c;
             while ((c = stream.read()) != -1) {
@@ -124,12 +80,13 @@ less.mode = 'rhino';
             return {
                 length: buffer.length,
                 toString: function(enc) {
+                    var javaString = Java.type("java.lang.String");
                     if (enc === 'base64') {
                         return encodeBase64Bytes(buffer);
                     } else if (enc) {
-                        return java.lang.String["(byte[],java.lang.String)"](buffer, enc);
+                        return new javaString(buffer, enc);
                     } else {
-                        return java.lang.String["(byte[])"](buffer);
+                        return new javaString(buffer);
                     }
                 }
             };
@@ -148,10 +105,13 @@ less.mode = 'rhino';
 
     function encodeBase64Bytes(bytes) {
         // requires at least a JRE Platform 6 (or JAXB 1.0 on the classpath)
-        return javax.xml.bind.DatatypeConverter.printBase64Binary(bytes)
+        var DatatypeConverter = Java.type("javax.xml.bind.DatatypeConverter");
+        return DatatypeConverter.printBase64Binary(bytes)
     }
     function encodeBase64String(str) {
-        return encodeBase64Bytes(new java.lang.String(str).getBytes());
+        var javaString = Java.type("java.lang.String");
+        var str = new javaString(str);
+        return encodeBase64Bytes(str.getBytes());
     }
 
 })();
